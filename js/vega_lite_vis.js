4e8c44fc17601d6acc7a5449d00e11bdadf7263a
Heun Yee Ching <yheu0001@student.monash.edu>
2025-10-13 17:15:14 +0800

    Add html and css file

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg_1 = "js/choropleth_map_rainfall_bar.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+
+
+
+var vg_3 = "js/rainfall_boxplot.vg.json";
+vegaEmbed("#boxplot", vg_3).then(function(result) {
+}).catch(console.error);
